5d7b0b5d6eb3501525c4e98771c892aa4d86e4fd
John D <john.davenport71@gmail.com>
2019-09-11 17:25:52 -0400

    Added README.md file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Make List README
+This file looks through the current directory for other folders and makes a link in a list that points to that folder.
+It also checks for an index file and if one is not found it looks for the first .php or .html file and makes a link directly to that file.
--- mkList.php
+++ mkList.php
@@ -1,11 +1,4 @@
 <?php
-    /*
-        This file looks through the current directory for other folders and
-        makes a link in a list that points to that folder.
-
-        It also checks for an index file and if one is not found it looks for the first
-        .php or .html file and makes a link directly to that file.
-    */
     $dir = dirname(__FILE__);
 
     $filePath = scandir($dir);
