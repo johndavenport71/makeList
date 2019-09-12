e53607dfb1965fbcf4fd96783410c9029be3aeaa
John D <john.davenport71@gmail.com>
2019-09-12 16:05:14 -0400

    First pass at refactoring.

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 This file looks through the current directory for other folders and makes a link in a list that points to that folder.
 It also checks for an index file and if one is not found it looks for the first .php or .html file and makes a link directly to that file.
 
-The file should be placed at the root of your site and will make all folders into a list. Changes need to be made so that it excludes image or script folders.
+The file should be placed at the root of your site and will make all folders that contain web files into a list of links.
 
 ## Example File Structure
 ROOT  
 |  
 |------Subfolder1  
-|       |------page2.php  
-|       |------index.php  
+|\t|------page2.php  
+|\t|------index.php  
 |-----Subfolder2  
-|       |------page1.php  
-|       |------page2.php  
+|\t|------page1.php  
+|\t|------page2.php  
 |-----index.php  
 |-----mkList.php  
--- mkList.php
+++ mkList.php
@@ -1,26 +1,45 @@
 <?php
     $dir = dirname(__FILE__);
+    //create array of the files in the directory
     $filePath = scandir($dir);
-    foreach($filePath as $path) {
-        $file = ltrim($path,'.');
+    
+    //Remove the first two elements of the filePath array : '.' and '..'
+    array_shift($filePath);
+    array_shift($filePath);
+    
+    foreach($filePath as $key => $file) {
+        //reset to root directory
         chdir($dir);
-        if (!strpos($file,'.') && $file != "") {
-            
+        
+        //check for a . in the file name
+        if (!strstr($file, '.')) {
+            //move into the folder
             chdir($file);
+
             $currentFiles = scandir(getcwd());
+            //search for an index file
             if (!findFile($currentFiles, 'index.')) {
+                //if there is no index file, look for any php or html file and link to that
                 foreach ($currentFiles as $current) {
                     if(strstr($current, '.php') || strstr($current, '.html')) {
                         print('<li><a href="' . $file . '/' . $current . '">' . $file . '</a></li>');
                         break;
                     }
-                }
+
+                }//end foreach
+
             } else {
+                //create a link to the current folder
                 print('<li><a href="' . $file . '">' . $file . '</a></li>');
+
             }//end if
+
         }//end if
+
     }//end foreach
     
+    //this function searches and array for a string and returns a boolean
+    //based on if the string was matched or not
     function findFile(array $directory, string $needle) {
         foreach ($directory as $dir) {
             $index = false;
